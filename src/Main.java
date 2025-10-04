c1d051279eee0fa5edb4d901d6eaad58c11bf2f7
alius <alius.s@kryptis.lt>
2025-10-04 16:33:31 +0300

    init

---
+++ src/Main.java
@@ -0,0 +1,68 @@
+import java.util.Random;
+
+/**
+ * Tema: Data Race problema - koncertų bilietų pirkimo sistema.
+ * <p>
+ * Aprašymas:
+ * Keli pirkėjai (gijos) tuo pačiu metu perka bilietus iš sistemos.
+ * Be sinchronizacijos gali įvykti "data race" - parduodama daugiau bilietų, nei turime.
+ * <p>
+ * Teisingas rezultatas: visi bilietai turi būti parduodami, bet ne daugiau, nei egzistuoja.
+ * Neteisingas scenarijus: išspausdinama, kad bilietų parduota daugiau, nei buvo.
+ *
+ * @author Alius Sultanovas
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        TicketCounter ticketCounter = new TicketCounter();
+
+        Runnable buyerTask = () -> {
+            String name = Thread.currentThread().getName();
+            while (ticketCounter.sellTicket(name)) {
+                try {
+                    Thread.sleep((int) (Math.random() * 100));
+                } catch (InterruptedException e) {
+                    //
+                }
+            }
+        };
+
+
+        //Sukuriame kelias gijas (pirkėjus)
+        Thread t1 = new Thread(buyerTask, "Pirkėjas-1");
+        Thread t2 = new Thread(buyerTask, "Pirkėjas-2");
+        Thread t3 = new Thread(buyerTask, "Pirkėjas-3");
+
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
+
+class TicketCounter {
+    private int tickets = 10;//Pradinis bilietų kiekis
+
+    //KRITINĖ VIETA. be "synchronized" kelios gijos (threads (pirkėjai)) tuo pat metu pakeičia "tickets" reikšmę
+    public boolean sellTicket(String buyerName) {
+    //public synchronized boolean sellTicket(String buyerName) {
+
+        int buyCount = 1;
+        try {
+            if (tickets > 0) {
+                //Prailginam laiką, kad geriau parodyt "data race" problemą, kad kuri nors gija nespėtų pakeist "tickets" reikšmės
+                //Thread.sleep((int) (Math.random() * 50));
+                Thread.sleep(new Random().nextInt(101)+50);//50 - min, 100 - max
+
+                tickets -= buyCount;
+
+                System.out.println(buyerName + " nuspirko " + buyCount + " bilietą(-us). Liko " + tickets + " vnt.");
+                return true;
+            }
+            throw new Exception(buyerName + " bandė pirkti bilietus, bet neužteko!");
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            return false;
+        }
+    }
+}
